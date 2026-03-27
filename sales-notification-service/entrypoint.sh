f1bb042db081bc06a4d2beea0ea941ae5420eb83
Sowrabh <sksseervi@gmail.com>
2026-03-27 11:02:12 +0000

    New: Added the Dockerfile, EntryPoint and requirements for notification service

---
+++ sales-notification-service/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo "Waiting for RabbitMQ..."
+
+until nc -z rabbitmq 5672
+do
+  echo "RabbitMQ not ready..."
+  sleep 2
+done
+
+echo "RabbitMQ is ready!"
+
+echo "Starting Celery worker..."
+
+celery -A app.core.celery_app.celery worker \
+  --loglevel=info \
+  -Q notification_queue
